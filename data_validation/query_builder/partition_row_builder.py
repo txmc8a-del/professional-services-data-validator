288c193e1e673a3641e81761da2557f9c03ccc7a
txmc8a-del <txmc8a@gmail.com>
2026-05-06 10:58:44 -0700

    Update partition_row_builder.py

--- data_validation/query_builder/partition_row_builder.py
+++ data_validation/query_builder/partition_row_builder.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+# Copyright 2026 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -6,35 +6,31 @@
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import ibis
+from typing import List
 from data_validation import clients
 from data_validation.query_builder.query_builder import QueryBuilder
 
-
-class PartitionRowBuilder(object):
+class PartitionRowBuilder:
     def __init__(
         self,
-        primary_keys: [str],
+        primary_keys: List[str],
         data_client: ibis.backends.base.BaseBackend,
         schema_name: str,
         table_name: str,
         custom_query: str,
         query_builder: QueryBuilder,
     ) -> None:
-        """Build a PartitionRowBuilder object which is ready to build a partition row filter query.
+        """Build a PartitionRowBuilder object to handle row-level validation filtering.
 
         Args:
-            primary_keys [str]: Keys used to identify a row for validation
-            data_client (BaseBackend): The Backend used to query random rows.
-            schema_name (String): The name of the schema for the given table.
-            table_name (String): The name of the table to query.
-            query_builder (QueryBuilder): QueryBuilder object.
+            primary_keys (List[str]): Keys used to identify a row for validation.
+            data_client (BaseBackend): The Ibis backend client.
+            schema_name (str): The name of the schema.
+            table_name (str): The name of the table.
+            custom_query (str): Optional custom SQL query.
+            query_builder (QueryBuilder): DVT QueryBuilder instance.
         """
         self.primary_keys = primary_keys
         self.query = self._compile_query(
@@ -49,23 +45,21 @@ class PartitionRowBuilder(object):
         custom_query: str,
         query_builder: QueryBuilder,
     ) -> ibis.Expr:
-        """Return an Ibis query object
-
-        Args:
-            data_client (BaseBackend): The Backend used to query random rows.
-            schema_name (String): The name of the schema for the given table.
-            table_name (String): The name of the table to query.
-            custom_query (String) : Custom query provided instead of a table
-            query_builder (QueryBuilder): QueryBuilder object.
-        """
+        """Compiles the initial Ibis expression with applied filters."""
         if table_name:
             table = clients.get_ibis_table(data_client, schema_name, table_name)
         else:
             table = clients.get_ibis_query(data_client, custom_query)
+            
+        # Apply filters defined in the QueryBuilder (e.g., WHERE clauses)
         compiled_filters = query_builder.compile_filter_fields(table)
-        filtered_table = table.filter(compiled_filters) if compiled_filters else table
-        return filtered_table
+        return table.filter(compiled_filters) if compiled_filters is not None else table
 
     def get_count(self) -> int:
-        """Return a count of rows of primary keys - they should be all distinct"""
-        return self.query[self.primary_keys].count().force_cast("int64").execute()
+        """Return a count of rows for the specified primary keys."""
+        # We select the primary keys and perform a count
+        return (
+            self.query[self.primary_keys]
+            .count()
+            .to_pandas() # Ensure compatibility across different Ibis backends
+        )
